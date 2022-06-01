94e02c5fa5e69a4de0eb7913c55931b295fb7c6b
Tsedexashu08 <ashutsedex@gmail.com>
2022-06-01 07:19:17 -0700

    loop

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+# 102-magic_calculation.py
+# Brennan D Baraban <375@holbertonschool.com>
+
+
 def magic_calculation(a, b, c):
     """Match bytecode provided by Holberton School."""
     if a < b:
@@ -6,4 +10,5 @@ def magic_calculation(a, b, c):
     if c > b:
         return (a + b)
     return (a*b - c)
+
 
