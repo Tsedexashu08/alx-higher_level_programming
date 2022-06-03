d202309c44cd84d17decf41bccddb2d3e8cd7f32
Tsedexashu08 <ashutsedex@gmail.com>
2022-06-03 03:53:08 -0700

    lists

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+#0-print_list_integer.py
+    """print all integers in a given list"""
+def print_list_integer(my_list=[]):
+    for i in range(len(my_list)):
+        print(f"{my_list[i]}")
