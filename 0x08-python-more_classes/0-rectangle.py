20f72ba5345d114a5442267506819a0c831f5e11
Tsedexashu08 <ashutsedex@gmail.com>
2022-06-29 03:18:21 -0700

    more classes

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# Tsedalu Ashenafi <Tsedexashu08@github.com>
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass
